3c7a6f94d61c4c6a845c1e5e4a2232e57c40fd83
OutLaw <progoutlaw@yandex.ru>
2024-11-22 20:52:01 +0300

    feat: fix up file

--- VideoDownloadService/src/main/java/com/example/videodownloadservice/services/VideoService.java
+++ VideoDownloadService/src/main/java/com/example/videodownloadservice/services/VideoService.java
@@ -9,13 +9,22 @@ import java.io.IOException;
 @Service
 public class VideoService {
 
-    private final String uploadDir = "...";
+    private final String uploadDir = "/Users/out_law/AppJava/VideoDownloadService/VideoDownloadService/";
 
     public String saveVideo(MultipartFile file) throws IOException {
-        String filePath = uploadDir + file.getOriginalFilename();
+        File dbDirectory = new File(uploadDir + "db/");
+
+        if (!dbDirectory.exists()) {
+            if (!dbDirectory.mkdirs()) {
+                throw new IOException("Не удалось создать директорию: " + dbDirectory.getAbsolutePath());
+            }
+        }
+
+        String filePath = dbDirectory.getAbsolutePath() + File.separator + file.getOriginalFilename();
         File dest = new File(filePath);
-        dest.getParentFile().mkdirs();
+
         file.transferTo(dest);
+
         return filePath;
     }
 }
